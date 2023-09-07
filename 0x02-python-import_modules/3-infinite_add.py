4d241dbdb4f6a9130b1a4a52e297928397adfa14
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-09-07 14:08:39 -0700

    fixing style

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     arglen = len(argv)
     sumation = 0
-    for n in range (1, arglen):
+    for n in range(1, arglen):
         sumation = int(argv[n]) + sumation
     print("{}".format(sumation))
-    
