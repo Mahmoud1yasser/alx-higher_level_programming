43100958d0add23fdb3961c97b7232c648186525
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-09-26 08:21:54 -0700

    update

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''0-square.py: Python script that creates a Square Class'''
+
+
+class Square:
+    '''Creates an empty Square type'''
+    def __init__(self, size=None):
+        """private attribute that defines the size of square.
+
+        Args:
+            self: fixed parameter
+            size: entered size of square
+
+        """
+            self.__size = size
+            if type(size) is not int:
+                raise TypeError('size must be an integer')
+            if size < 0:
+                raise ValueError('size must be >= 0')
+
