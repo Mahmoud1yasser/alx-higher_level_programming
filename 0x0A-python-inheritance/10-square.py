7e315cc072d293e9eb53ae5778b850b786a7cc3a
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-10-09 12:05:31 -0700

    update

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-'''9-base_geometry.py'''
-
+"""
+Square module
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """class inherits from Rectangle"""
+
     def __init__(self, size):
+        """initialization method"""
         self.integer_validator('size', size)
         self.__size = size
-
-    def area(self):
-        return self.size ** 2
+        super().__init__(self.__size, self.__size)
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-'''7-base_geometry.py'''
+"""
+class module
+"""
 
 
 class BaseGeometry:
-    '''
-    class BaseGeometry
-    '''
-
-    def __init__(self):
-        pass
+    """Geometry class"""
 
     def area(self):
-        '''
-        raise an exception
-        '''
-        raise Exception("area() is not implemented")
+        """raise exception is area is not implemented"""
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        '''
-        check value input is correct type
-        '''
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        """check if value is an integer"""
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
