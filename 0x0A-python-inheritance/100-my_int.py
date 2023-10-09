5c383eacaf78643d172899a4d6507c620539368c
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-10-09 12:21:02 -0700

    update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,9 +7,9 @@ class module
 class MyInt(int):
     """class with int object"""
 
-    def __ee__(self, other):
+    def __eq__(self, other):
         """equal equal method"""
-        return super().__ee__(other)
+        return super().__eq__(other)
 
     def __ne__(self, other):
         """not equal method"""
