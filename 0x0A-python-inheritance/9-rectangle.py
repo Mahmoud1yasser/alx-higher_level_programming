2a073f92785fe99e07e5ad04262f89b4b1d0b552
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-10-09 12:00:47 -0700

    update

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-'''9-base_geometry.py'''
-
+"""
+Class Module
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    '''class Rectangle inherits from BaseGeometry'''
+    """Rectangle subclass"""
 
-    def __init__(self, width=0, height=0):
-        '''private instantiation of attributes
-           validate type with integer_validator
-        '''
+    def __init__(self, width, height):
+        """initialization method"""
         self.integer_validator('width', width)
         self.integer_validator('height', height)
         self.__width = width
         self.__height = height
 
-    def __str__(self):
-        '''informal representation of a Rectangle'''
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def __repr__(self):
-        '''formal representation of a Rectangle'''
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
     def area(self):
+        """calculate area of width and height"""
         return self.__width * self.__height
+
+    def __str__(self):
+        """print method"""
+        return "[{}] {}/{}".format(__class__.__name__,
+                                   self.__width, self.__height)
