c0b43e56cd337d1cb488008ceb256674021c127a
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-10-10 11:08:44 -0700

    update

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# 9-student.py
+'''Student to JSON '''
+
+
+class Student:
+    '''defines a student by first_name, last_name,age'''
+
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        for items in attrs:
+            if hasattr(self, items):
+                my_dict[items] = getattr(self, items)
+        return my_dict
