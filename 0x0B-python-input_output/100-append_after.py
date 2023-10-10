97fd1aed29219601237f8a9d89693957652230a1
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-10-10 11:39:25 -0700

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,6 +2,7 @@
 # 100-append_after.py
 '''Search and update '''
 
+
 def append_after(filename="", search_string="", new_string=""):
     '''function that inserts a line of text to a file,
     after each line containing a specific string'''
