def6cdb5a450ff6350afeba7c531b1229e665fec
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-10-15 08:30:27 -0700

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 '''class to mange id attribute to avoid dublicating'''
 
 
+import json
+
+
 class Base:
     ''' Base class for all other classes in project'''
     __nb_objects = 0
@@ -12,5 +15,36 @@ class Base:
         if id is not None:
             Base.id = id
         else:
-           Base.__nb_objects += 1
-           Base.id = Base.__nb_objects
+            Base.__nb_objects += 1
+            Base.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Dictionary to JSON string
+        Returns:returns the JSON string
+        representation of list_dictionaries
+        '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Method that writes JSON string representation to file'''
+        if list_objs is None:
+            list_objs = []
+        list_dictionaries = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(list_dictionaries)
+        filename = f"{cls.__name__}.json"
+        with open(filename, 'w') as json_file:
+            json_file.write(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''class method that prints instances'''
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
