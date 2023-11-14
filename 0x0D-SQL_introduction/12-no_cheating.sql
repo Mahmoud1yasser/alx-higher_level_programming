337073cc2d9907cf1ebfd4db4d0fdde0e82a46e0
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-11-14 12:25:44 -0800

    update

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- script that updates the score of Bob to 10 in the table second_table.
+SELECT score, name FROM second_table
+ORDER BY score DESC;
+UPDATE second_table SET score = 10
+WHERE name = 'BOb'
