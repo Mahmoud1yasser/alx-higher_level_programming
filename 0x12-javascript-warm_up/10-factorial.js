c4604afcead07d0d1f3f3e4473aa2fce0649942d
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-11 12:09:18 -0800

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // JS to compute the factorial of a number
 'use strict';
-let a = Number(process.argv[2]);
+const a = Number(process.argv[2]);
 function factorialize (a) {
   if (isNaN(a) || a === 1) {
     return (1);
