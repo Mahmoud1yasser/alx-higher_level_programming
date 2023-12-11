260170d8d60102813be80ea99ef6472e009c2ff9
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-11 11:19:32 -0800

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const firstArgumentAsInt = parseInt(process.argv[2], 10);
 if (!isNaN(firstArgumentAsInt)) {
-    console.log('My number:', firstArgumentAsInt);
-  } else {
-    console.log('Not a number');
-  }
+  console.log('My number:', firstArgumentAsInt);
+} else {
+  console.log('Not a number');
+}
