5f06c3034fa981c5f487a915da00952f1bb9abec
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-11 12:27:40 -0800

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,2 @@
 #!/usr/bin/node
-
-if (process.argv.length > 3) {
-  let numArray = [];
-  let j = 0;
-  for (let i = 2; i < process.argv.length; i++) {
-    numArray[j] = process.argv[i];
-    j++;
-  }
-  let largest = numArray[0];
-  let nextLargest = numArray[1];
-  for (let i = 0; i < numArray.length; i++) {
-    if (numArray[i] > largest) {
-      nextLargest = largest;
-      largest = numArray[i];
-    } else if (numArray[i] > nextLargest && numArray[i] < largest) {
-      nextLargest = numArray[i];
-    }
-  }
-  console.log(nextLargest);
-} else {
-  console.log(0);
-}
+console.log(process.argv.length < 4 ? 0 : process.argv.slice(2).sort((a, b) => b - a)[1]);
