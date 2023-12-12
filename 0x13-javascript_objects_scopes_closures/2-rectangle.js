b789c797d02cc4febc74d2f211e96e1f4703e25b
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-12 08:36:51 -0800

    update

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
+/* class Rectangle that defines a rectangle:
+ */
 'use strict';
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       return {};
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 module.exports = Rectangle;
