ea325cdef79e1677b5d41d7f5f98643c17eb061d
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-12 09:28:11 -0800

    update

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const newSquare = require('./5-square');
+lass Square extends newSquare {
+  charPrint (c) {
+    for (let count = 0; count < this.height; count++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
