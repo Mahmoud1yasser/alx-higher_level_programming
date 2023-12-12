02acfeefdd383c3966d47970543060a1d3e647d8
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-12 08:48:40 -0800

    update

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* class Rectangle that defines a rectangle:
+ */
+'use strict';
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    let i;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
