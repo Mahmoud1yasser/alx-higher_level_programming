cce9f1acd1ee7877707e6a8279f60ec7f836ef1b
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-16 09:40:35 -0800

    update

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,12 +6,15 @@ Python Script to create a class State
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """Creates the class State"""
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
